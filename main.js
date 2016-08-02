794b4a819a0ba15c3e5aedd3f18e54fb32ba516c
Jealousmango <jealousmango@gmail.com>
2016-08-02 14:49:41 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function Deck() {
+    function Card(suit, count) {
+        this.suit = suit;
+        this.count = count;
+    }
+    this.cards = [];
+    this.shuffle = function() {};
+    this.reset = function() {};
+    this.deal = function() {}:
+
+return this;
+};
+
+function Player(name) {
+
+}
